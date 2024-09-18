6900dfc43054d756a24a23e43f0d6ec833145ab8
Msch167 <bulychevm@unimelb.edu.au>
2024-09-18 10:20:03 +1000

    Update 8.03.c

--- 8.03.c
+++ 8.03.c
@@ -1,8 +1,3 @@
-/* Exercise 8.03: Polygons and perimeters
- * Alistair Moffat, May 2014
- * Liam Saliba, December 2021
- * (c) University of Melbourne */
-
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
@@ -18,7 +13,7 @@ typedef struct {
 #define MAXPTS 100
 #define MINPTS 3
 typedef struct {
-    vector_t pts[MAXPTS]; // struct in struct
+    vector_t pts[MAXPTS];
     int npts; /* the buddy variable */
 } poly_t;
 
@@ -27,8 +22,6 @@ double distance(vector_t, vector_t);
 double perimeter(poly_t P);
 int read_vector(vector_t *pt);
 poly_t read_poly();
-
-## zusammen machen
 
 int
 main(int argc, char *argv[]) {
@@ -53,6 +46,7 @@ perimeter(poly_t P) {
 }
 
 /* reuse the function we've already written! */
+
 /* return the Euclidean distance between the given vectors (ex8-02 soln.) */
 double
 distance(vector_t p1, vector_t p2) {
@@ -66,7 +60,7 @@ distance(vector_t p1, vector_t p2) {
 // reads in a point from input and returns 1 if successful
 int
 read_vector(vector_t *pt) {
-    ...
+    return scanf("%lf , %lf", &pt->x, &pt->y) == 2;
 }
 
 // reads in a polygon from input and returns 1 if successful
@@ -75,6 +69,9 @@ read_poly() {
     poly_t poly;
     poly.npts = 0;
     // read in points until EOF is read.
-    ...
+    while (poly.npts < MAXPTS && read_vector(&poly.pts[poly.npts])) {
+        ++poly.npts;
+    }
+    assert(poly.npts >= MINPTS);
     return poly;
 }
